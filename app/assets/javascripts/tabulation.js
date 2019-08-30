5baae5ef9b4411a9984af4643d91a7f1b3931155
Rianasoa <rrianasoaonimalala@gmail.com>
2019-08-30 17:28:30 +0300

    ajax4

--- app/assets/javascripts/tabulation.js
+++ app/assets/javascripts/tabulation.js
@@ -3,75 +3,75 @@ var slides = [];
 
 $(document).ready(function(){
 
-    $('.emailbody').hide();
-      $('.emailobject').click(function(){  
-        $('.emailbody').hide(); 
-        $(this).next().slideToggle();
-});
+//     $('.emailbody').hide();
+//       $('.emailobject').click(function(){  
+//         $('.emailbody').hide(); 
+//         $(this).next().slideToggle();
+// });
 	
-//   let title = $('.category-title');
-// 	let content = title.next();
-// 	content.hide();
-// 	title.on('click',function(){
-// 		content.hide();
-// 		$(this).next().show();
-// 	});
+  let title = $('.category-title');
+	let content = title.next();
+	content.hide();
+	title.on('click',function(){
+		content.hide();
+		$(this).next().show();
+	});
 
-// 	let avat = $('.avatar');
-// 	let menu = $('.dropdown-content')
-// 	menu.hide()
-// 	avat.on('click', function() {
-// 		menu.toggle();
-// 	});
+	let avat = $('.avatar');
+	let menu = $('.dropdown-content')
+	menu.hide()
+	avat.on('click', function() {
+		menu.toggle();
+	});
 
-// 	// Get the modal
-// 	let modal = document.getElementById("myModal");
+	// Get the modal
+	let modal = document.getElementById("myModal");
 
-// 	// Get the button that opens the modal
-// 	let signup = document.getElementById("signup");
+	// Get the button that opens the modal
+	let signup = document.getElementById("signup");
 
-// 	// Get the <span> element that closes the modal
-// 	let span = document.getElementsByClassName("close")[0];
+	// Get the <span> element that closes the modal
+	let span = document.getElementsByClassName("close")[0];
 
-// 	// When the user clicks on the button, open the modal 
-// 	signup.addEventListener('click',function(e){
-// 		e.preventDefault();
-// 		modal.style.display = "block";
-// 	});
+	// When the user clicks on the button, open the modal 
+	signup.addEventListener('click',function(e){
+		e.preventDefault();
+		modal.style.display = "block";
+	});
 
-// 	// When the user clicks on <span> (x), close the modal
-// 	span.addEventListener('click',function(e){
-// 		e.preventDefault();
-// 		modal.style.display = "none";
-// 	});
+	// When the user clicks on <span> (x), close the modal
+	span.addEventListener('click',function(e){
+		e.preventDefault();
+		modal.style.display = "none";
+	});
 
-// 	let modal1 = document.getElementById("myModal1");
+	let modal1 = document.getElementById("myModal1");
 
-// 	// Get the button that opens the modal
-// 	let login = document.getElementById("login");
+	// Get the button that opens the modal
+	let login = document.getElementById("login");
 
-// 	let span1 = document.getElementsByClassName("close")[1];
+	let span1 = document.getElementsByClassName("close")[1];
 
-// 	login.addEventListener('click',function(e){
-// 		e.preventDefault();
-// 		modal1.style.display = "block";
-// 	});
+	login.addEventListener('click',function(e){
+		e.preventDefault();
+		modal1.style.display = "block";
+	});
 
-// 	// When the user clicks on <span> (x), close the modal
-// 	span1.addEventListener('click',function(e){
-// 		e.preventDefault();
-// 		modal1.style.display = "none";
-// 	});
+	// When the user clicks on <span> (x), close the modal
+	span1.addEventListener('click',function(e){
+		e.preventDefault();
+		modal1.style.display = "none";
+	});
 
 
-// 	// When the user clicks anywhere outside of the modal, close it
-// 	window.onclick = function(event) {
-// 	  if (event.target == modal) {
-// 	    modal.style.display = "none";
-// 	  }else if(event.target == modal1){
-// 	  	modal1.style.display = "none";
-// 	  }
-// 	};
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }else if(event.target == modal1){
+	  	modal1.style.display = "none";
+	  }
+	};
 
 // //Carroussel
 	
--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,22 +1,49 @@
 class EmailsController < ApplicationController
-  def index
-  	@emails = Email.all
-  end
+	def index
+	  	@emails = Email.all
+	end
 
-  def show
-  	@email = Email.find(params[:id])
-    respond_to do |format|
-        format.html {redirect_to emails_path}   
-        format.js { } 
-    end  
-  end
+	def show
+		@email = Email.find(params[:id])
 
-  def create
-  	@email = Email.create!(object: Faker::Book.title,body: Faker::Lorem.paragraph)
+		@email.update(read: true)
+	    respond_to do |format|
+	        format.html {redirect_to emails_path}   
+	        format.js { } 
+	    end  
+	end
 
-	respond_to do |format|
-        format.html {redirect_to emails_path}   
-        format.js { } 
-    end  
-  end
+	def create
+	  	@email = Email.create!(object: Faker::Book.title,body: Faker::Lorem.paragraph)
+
+		respond_to do |format|
+	        format.html {redirect_to emails_path}   
+	        format.js { } 
+	    end  
+	end
+	def update
+	    @email = Email.find(params[:id])
+
+
+	    if @email.read == true
+	    	@email.update(read: false)
+	    else 
+	    	@email.update(read: true)
+	    end	
+
+	    respond_to do |format|
+	      format.html{ redirect_to email_index_path }
+	      format.js{ }
+	    end
+  	end
+	def destroy
+	    @email = Email.find(params[:id])
+	    @email.destroy
+	    respond_to do |format|
+	      format.html{
+	        redirect_to root_path
+	      }
+	      format.js{}
+	    end
+  	end
 end
